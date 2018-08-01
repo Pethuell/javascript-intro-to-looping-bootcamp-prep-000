248126d733e9687cbac65b232f56fd4f86576454
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-08-01 13:09:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(aray) {
   for(var i = 0; i < 25; i++) {
     if(i===1) {
-      aray.push("I am " + i + "strange loop.");
+      aray.push("I am 1 strange loop.");
     } else {
       aray.push("I am " + i + " strange loops.");
     }
